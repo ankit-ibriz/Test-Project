bab2656eb3090aadc89d1744888ed3fdc6531a3f
Ankit <ankit.ibriz@gmail.com>
2023-07-03 10:48:03 +0545

    Backed added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 function App() {
+  const [userName, setUsername] = useState('')
+
+  useEffect(() => {
+    getNames();
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUsername(response.data);
+  }
+
   return (
     <div>
       <h1>Testing react app CI/CD and its deployment in AWS S3, Cloudfront, Route53 V3</h1>
+      <h2>If my name displays then backend is working properly: {userName}</h2>
       <nav
         style={{
           borderBottom: "solid 1px",
